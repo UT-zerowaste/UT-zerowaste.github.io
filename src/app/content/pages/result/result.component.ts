45e94dd25570345a77e4c8d0371740ed920e275f
BLUE110601 <57922440+BLUE110601@users.noreply.github.com>
2022-09-18 20:01:02 -0500

    Added a completion column to keep track of the amount of times a user finishes

--- src/app/content/pages/result/result.component.ts
+++ src/app/content/pages/result/result.component.ts
@@ -7,39 +7,35 @@ import { UserService } from 'src/services/user.service';
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent {
   worldImpactMsg: string = "";
   points: number = 5;
   showPointsPage: boolean = false;
   updateUserObject = {};
 
+  // Used if the user decides to clear their local storage for some reason
   DFAULT_WORLD_IMPACT_ID = "1";
+
   constructor(private selectionService: SelectionService, private userService: UserService) {
     this.userService.user$.subscribe(user => {
       if (user.shows_ranking == null) {
-        console.log('null called')
-        this.showPointsPage = Math.random() >= 0.5 ? true : false;
-        this.updateUserObject = { ...this.updateUserObject, shows_ranking: this.showPointsPage }
+        this.showPointsPage = Math.random() > 0.5 ? true : false;
+        this.updateUserObject = { ...this.updateUserObject, shows_ranking: this.showPointsPage };
       } else {
         this.showPointsPage = user.shows_ranking;
       }
 
       if (this.showPointsPage) {
-
-        this.updateUserObject = { ...this.updateUserObject, finish_count: user.finish_count + this.points }
-        this.userService.updateUser(this.updateUserObject);
-  
+        this.updateUserObject = { ...this.updateUserObject, finish_count: user.finish_count + this.points };
       } else {
-  
         let worldImpactId = parseInt(localStorage.getItem("worldImpactId") ?? this.DFAULT_WORLD_IMPACT_ID);
         this.selectionService.getWorldImpact(worldImpactId).then(worldImpact => {
           this.worldImpactMsg = worldImpact.message;
         });
-
       }
+
+      this.updateUserObject = { ...this.updateUserObject, successful_completions: user.successful_completions + 1 };
+      this.userService.updateUser(this.updateUserObject);
     });
   }
-
-  ngOnInit(): void { }
-
 }
